7a3ba1b7f9ecdc4f1fbe855bec420a2b014740fb
Tanunchai-Dumruttana <88667996+Tanunchai-Dumruttana@users.noreply.github.com>
2021-08-11 00:12:37 +0200

    Add files via upload

---
+++ lab.js
@@ -0,0 +1,29 @@
+// Iteration #1: Find the maximum
+
+function maxOfTwoNumbers(AA, BB){
+  
+  
+    if (AA < BB){
+      
+      console.log(`The largest number is : ${BB}`)
+      
+    }
+    
+    else if (AA > BB){
+      
+      console.log(`The largest number is : ${AA}`)
+      
+    }
+     else {
+       
+       console.log(`The largest number is : ${AA}`)
+       
+     }
+    
+    
+   
+  }
+  
+  maxOfTwoNumbers(6, 9);
+  
+  // 'The largest number is : 9'
